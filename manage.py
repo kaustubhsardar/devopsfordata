96803431b2604e2b8843647a1d381c9f0317eeba
Kaustubh Milindrao Sardar <kaustubhsardar@gmail.com>
2020-06-19 12:38:15 -0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 import os
-from demo_app.app import create_app, test,test_db
+from demo_app.app import create_app
 app = create_app()
 app.app_context().push()
-test()
-test_db()
+
+app.test()
+app.test_db()
