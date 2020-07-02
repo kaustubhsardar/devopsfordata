ab5b4eae5327996cb0562c889abb46f9afbfd000
Kaustubh Milindrao Sardar <kaustubhsardar@gmail.com>
2020-07-02 13:28:04 -0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -15,6 +15,7 @@ manager.add_command('db', MigrateCommand)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128))
-
+    age = db.Column(db.Integer)
+    
 if __name__ == '__main__':
     manager.run()
