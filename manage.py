997b9bb4d681b2ed811c6354291153c4259a3254
Kaustubh Milindrao Sardar <kaustubhsardar@gmail.com>
2020-07-02 10:35:27 -0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,5 +1,20 @@
-# import os
-# from demo_app.app import create_app
-  
-import os
-os.system('python demo_app/app.py')
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+
+if __name__ == '__main__':
+    manager.run()
