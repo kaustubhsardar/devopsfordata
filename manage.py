02d43f915e3d79faad6bb4875d899f95263d22b1
Kaustubh Milindrao Sardar <kaustubhsardar@gmail.com>
2020-07-02 13:35:03 -0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 # engine = SQLAlchemy.create_engine("postgres://kaustubh:12345@localhost:3308/demo_db")
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://kaustubh:12345@localhost:3308/demo_db'
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://kaustubh:12345@localhost/demo_db'
 
 db = SQLAlchemy(app)
 sa_url='postgres://kaustubh:12345@localhost:3308/demo_db'
