6643f735c719fcec129bd5d08851cb6e967aea26
Kaustubh Milindrao Sardar <kaustubhsardar@gmail.com>
2020-07-02 11:04:27 -0700

    Update upgrade_test1.py

--- Dev/upgrade/upgrade_test1.py
+++ Dev/upgrade/upgrade_test1.py
@@ -1,1 +1,9 @@
+import os
 
+
+command1 = os.system("python manage.py db init")
+command2=os.system("python manage.py db migrate")
+command3=os.system("python manage.py db upgrade")
+command4=os.system("python manage.py db downgrade")
+
+print(command1+"\n"+command2+"\n"+command3+"\n"+command4)
